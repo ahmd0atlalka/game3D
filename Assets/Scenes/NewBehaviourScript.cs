8276848c070b326b48a1bfc12cc514f390000769
ahmd0atlalka <49496983+ahmd0atlalka@users.noreply.github.com>
2019-11-10 17:04:00 +0200

    file commit

---
+++ Assets/Scenes/NewBehaviourScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+
+    public float speed = 6.0f;
+    // Update is called once per frame
+   private void Update()
+    {
+        if(Input.GetKey(KeyCode.W))
+        {
+            transform.position -= transform.forward * speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.position += transform.forward * speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position -= transform.right * speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.position += transform.right* speed * Time.deltaTime;
+        }
+    }
+}
